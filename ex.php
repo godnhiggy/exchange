81dd8569653e956247b52e498985ab6d9491f079
higgy <godnhiggy@gmail.com>
2020-07-05 17:42:32 -0400

    not sure what the deal is with this update

--- ex.php
+++ ex.php
@@ -9,12 +9,12 @@
     <h1>This is the second edit</h1>
     This is an edit on the remote machine that allows us to check and see if<br>
     the push will work if there are changes on a different file than the one edited on the simple machine
-    
+
     <br>
-    Lets try this again 
-    
-    <h1>One more time.......</h1>
-    
-    
+    Lets try this again
+
+
+
+
   </body>
 </html>
