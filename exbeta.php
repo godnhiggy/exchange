e07dde7c8a46e47bfda85a1aba345d8a0131ec0b
higgy <godnhiggy@gmail.com>
2020-07-05 17:13:12 -0400

    getting ready to push two files after a recursive method applied to a pull of two files edited on different machines ie local and remote

--- exbeta.php
+++ exbeta.php
@@ -8,6 +8,9 @@
     nothing here yet
     ////first edit on local machine for this <fieldset>
       <h1>Lets get this done correctly</h1>
+      <h2>adding some more stuff after recursive method used to update remote machine file that had not been  changed <br>
+        except by the local machines edit. The recursive method seems to <br>
+        mean that if changes have been made to both local and remote but they are on different files, the push goes through recursively?
     </fieldset>
   </body>
 </html>
