ec3f647bc790db2d43b00bef076e1465a912f9a4
Coach Higgy <godnhiggy@gmail.com>
2020-07-05 16:43:51 -0400

    Update remote_created.php

--- remote_created.php
+++ remote_created.php
@@ -7,5 +7,8 @@
   <body>
    <h2>Created on remote</h2>
    This is the first edit on local machine for this file.
+    <br>
+    ////this is the first edit on the remote machine for this file.
+    <h3>Hello World</h3>
   </body>
 </html>
